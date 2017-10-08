5e5661347bc47105ee8a33fe4150e5837d98cfcb
TitanSnow <sweeto@live.cn>
2017-10-08 20:00:18 +0800

    add parallel_read_safe

--- diktat/__init__.py
+++ diktat/__init__.py
@@ -5,5 +5,7 @@ import pkg_resources
 def setup(app):
     app.add_html_theme('diktat', os.path.dirname(__file__))
     return {
-        'version': pkg_resources.get_distribution('diktat').version
+        'version': pkg_resources.get_distribution('diktat').version,
+        'parallel_read_safe': True,
+        'parallel_write_safe': True,
     }
